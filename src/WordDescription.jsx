183fc105c5f29f646cbbc1b3881f63c8c654deb6
Naz <nazeefsuleman@gmail.com>
2023-02-16 08:25:06 -0500

    Added styles to WordDescription component

--- src/WordDescription.jsx
+++ src/WordDescription.jsx
@@ -1,24 +1,31 @@
-export default function WordDescription(){
-    return (
-        <div className="WordDescription">
-          <h2>Word type(ex:type)</h2>
-          <div className="definition">
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-                suscipit porro dolor ut dicta illum quasi tempora fuga rerum.
-                Eum, eveniet veritatis consequatur voluptas eligendi iste
-                maiores aut ut voluptatibus.
-              </li>
-              <li>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-                suscipit porro dolor ut dicta.
-              </li>
-            </ul>
-            <div className="synonyms">
-              blalalalala
-            </div>
-          </div>
+import "./WordDescription.css";
+export default function WordDescription() {
+  return (
+    <div className="WordDescription">
+      <div className="type">
+        Word type(ex:type)
+        <hr />
+      </div>
+      <div className="definition">
+        <h3>Meaning</h3>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            suscipit porro dolor ut dicta illum quasi tempora fuga rerum. Eum,
+            eveniet veritatis consequatur voluptas eligendi iste maiores aut ut
+            voluptatibus.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            suscipit porro dolor ut dicta.
+          </li>
+        </ul>
+        <div className="synonyms">
+          <p>
+            Synonyms:<span>blablabls</span>
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
