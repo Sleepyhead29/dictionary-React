5d7707399fbf94d0d66473ee301d9764d3c2fb53
Naz <nazeefsuleman@gmail.com>
2023-02-15 21:44:34 -0500

    Added styles to UserInput component

--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,6 +1,7 @@
+import './UserInput.css';
 export default function UserInput(){
     return(
-        <form action="GET">
+        <form action="GET" className='UserInput'>
           <input type="text" placeholder="Search for a word"/>
         </form>
     )
