2089e847195d70699ff7e271c8e1fc44e7908907
Naz <nazeefsuleman@gmail.com>
2023-02-15 21:53:12 -0500

    Added styles to WordSearched component

--- src/WordSearched.jsx
+++ src/WordSearched.jsx
@@ -1,11 +1,12 @@
+import './WordSearched.css';
 export default function WordSearched(){
     return (
         <div className="WordSearched">
           <div className="word">
             <h1>Hello</h1>
             <p>In latin</p>
-            <button>Play Sound</button>
           </div>
+          <button>Play Sound</button>
         </div>
     )
 }
