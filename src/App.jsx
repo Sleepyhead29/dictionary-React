32d4aeed83bd8e7e3474c25d142bf3dd23129ae0
Naz <nazeefsuleman@gmail.com>
2023-02-15 21:04:22 -0500

    Created header with no styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+    <header>
+      <h1>Logo</h1>
+      <div className="displayOptions">
+        <div className="textFont">changeFont</div>
+        <div className="appearence">Dark Mode</div>
+      </div>
+      <form action="GET">
+        <input type="text" />
+      </form>
+      
+    </header>
     </div>
   );
 }
