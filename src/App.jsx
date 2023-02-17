ce50d8536b5026d2e36bd66325ee5ac074fa34d9
Naz <nazeefsuleman@gmail.com>
2023-02-17 09:40:05 -0500

    Connected to dictionary api

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import Header from "./Header";
 import UserInput from "./UserInput";
 import WordSearched from "./WordSearched";
 import WordDescription from "./WordDescription";
+import { useState,useEffect } from "react";
 function App() {
+  // Track current searched word
+  const [currentWord,setCurrentWord] = useState('');
+  // Track current definitionin
+  const [searchedWord,setSearchWord] = useState();
+  // Fetches the current word in the API and returns it's value
+  async function fetchWordDictionary(currentWord){
+    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`);
+    let result = await response.json();
+    console.log(result);
+    return result;
+  }
+  useEffect(() => {
+   fetchWordDictionary(currentWord);
+    
+  }, [currentWord])
+  
   return (
     <div className="App">
       <Header/>
-      <UserInput/>
+      <UserInput currentWord = {currentWord} setCurrentWord = {setCurrentWord} fetchWordDictionary = {fetchWordDictionary}/>
       <WordSearched/>
       <WordDescription/>
       <hr />
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,8 +1,16 @@
+import { useRef } from 'react';
 import './UserInput.css';
-export default function UserInput(){
+
+export default function UserInput({currentWord,setCurrentWord,fetchWordDictionary}){
+  // Fetch word in dictionary only when form is submitted
+  const userInput = useRef();
+function handleSearch(e){
+  e.preventDefault();
+setCurrentWord(userInput.current.value);
+}
     return(
-        <form action="GET" className='UserInput'>
-          <input type="text" placeholder="Search for a word"/>
+        <form action="GET" className='UserInput' onSubmit={handleSearch}>
+          <input type="text" placeholder="Search for a word"  ref = {userInput} onChange={(e)=> userInput.current.value = e.target.value}/>
         </form>
     )
 }
