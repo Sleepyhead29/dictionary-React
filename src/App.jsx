bbfde4e73774830cc0c26c1e3d661170b81306b0
Naz <nazeefsuleman@gmail.com>
2023-02-23 08:19:20 -0500

    Added a NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,51 @@ import Header from "./Header";
 import UserInput from "./UserInput";
 import WordSearched from "./WordSearched";
 import WordDescription from "./WordDescription";
-import { useState,useEffect } from "react";
+import NotFound from "./NotFound";
+import { useState, useEffect } from "react";
 function App() {
   // Track current searched word
-  const [currentWord,setCurrentWord] = useState('');
+  const [currentWord, setCurrentWord] = useState("");
   // Track current definition
-  const [searchedWord,setSearchedWord] = useState([]);
+  const [searchedWord, setSearchedWord] = useState([]);
+  // Track fetch responseState
+  const [responseState, setResponseState] = useState();
   // Fetches the current word in the API and returns it's value
-  async function fetchWordDictionary(currentWord){
-    if(currentWord){
-      let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`);
-    let result = await response.json();
-    setSearchedWord(result);
-    console.log(result);
-    return result;
+  async function fetchWordDictionary(currentWord) {
+    if (currentWord) {
+      let response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`
+      );
+
+      setResponseState(response.status);
+      console.log(response.status);
+      let result = await response.json();
+      setSearchedWord(result);
+      console.log(result);
+      return result;
     }
-    
   }
   useEffect(() => {
-   fetchWordDictionary(currentWord);
-  }, [currentWord])
-  
+    fetchWordDictionary(currentWord);
+  }, [currentWord]);
+  useEffect(() => {
+    console.log(searchedWord);
+  }, [searchedWord]);
+  useEffect(() => {
+    console.log(responseState);
+  }, [responseState]);
+  //Array.isArray(searchedWord)
   return (
     <div className="App">
-      <Header/>
-      <UserInput setCurrentWord = {setCurrentWord}/>
-      {searchedWord.map(word => {
-        return <WordSearched  key = {Math.random()} searchedWord = {word}/>
-      })}
+      <Header />
+      <UserInput setCurrentWord={setCurrentWord} />
+      {responseState === 200 ? (
+        searchedWord.map((word) => {
+          return <WordSearched key={Math.random()} searchedWord={word} />;
+        })
+      ) : (
+        <NotFound noDef={searchedWord.title} />
+      )}
     </div>
   );
 }
---
+++ src/NotFound.jsx
@@ -0,0 +1,5 @@
+export default function NotFound({noDef}){
+    return (
+        <h1>{noDef}</h1>
+    )
+}
